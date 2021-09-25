8e367c523e2c594e57e577ba2bd6e2cc5332c481
RecklessPotcover <alexander2d6@outlook.com>
2021-09-25 13:54:14 +0800

    Created 'Add a new post' button for 'My Posts' section

--- src/pages/profile/components/MyPosts.tsx
+++ src/pages/profile/components/MyPosts.tsx
@@ -1,18 +1,29 @@
-import { Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import { makeStyles } from '@material-ui/core/styles';
 
 import PostCard from '../../../utils/post-card';
 
+const useStyles = makeStyles({
+  button: {
+    border: '2px solid lightgrey',
+    borderRadius: 6,
+  },
+});
 export default function MyPosts(props: any) {
+  const classes = useStyles();
+
   const { posts } = props;
 
   const postCards = posts.map((item: any) => {
     return (
-      <Grid item>
+      <Grid item key={item.title}>
         <PostCard
           key={item.title}
           coverURL={item.postCover}
           title={item.title}
           description={item.content}
+          time={item.createdAt}
         />
       </Grid>
     );
@@ -20,6 +31,11 @@ export default function MyPosts(props: any) {
 
   return (
     <Grid container direction="column" spacing={4}>
+      <Grid item xs>
+        <Button className={classes.button} startIcon={<AddIcon />}>
+          Create a new post
+        </Button>
+      </Grid>
       {postCards}
     </Grid>
   );

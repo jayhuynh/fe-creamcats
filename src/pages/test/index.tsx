b43cab35efa8c0b3237bfbfbb006a307616523d2
RecklessPotcover <alexander2d6@outlook.com>
2021-10-09 15:13:22 +0800

    Make 'Applicaions' an isolated component

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,29 +1,9 @@
-import { Grid } from '@material-ui/core';
-import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
-import ApplicationSearchArea from '../profile/components/applications/ApplicationSearchArea';
-import ApplicationTable from '../profile/components/applications/ApplicationTable';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#fa6980',
-    },
-    secondary: {
-      main: '#bfc4c9',
-    },
-  },
-});
+import Applications from '../profile/components/applications';
 
 export default function Test() {
-  // Where should I get the organization ID?
-  const organizationId = 1;
-
   return (
-    <MuiThemeProvider theme={theme}>
-      <Grid container direction="column" spacing={4} alignItems="flex-start">
-        <ApplicationSearchArea organizationId={organizationId} />
-        <ApplicationTable organizationId={organizationId} />
-      </Grid>
-    </MuiThemeProvider>
+    <div>
+      <Applications />
+    </div>
   );
 }

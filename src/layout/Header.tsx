8db41e62848b9dacfbfc523f915d9c8f230d26ad
GavintheLionHeart <zhuhe1993@gmail.com>
2021-10-24 16:21:58 +0800

    fix the header z_index

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -8,14 +8,9 @@ import Grow from '@material-ui/core/Grow';
 import Paper from '@material-ui/core/Paper';
 import Popper from '@material-ui/core/Popper';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import { fromAuth, fromProfile, useAppDispatch } from '../store';
+import { fromAuth, useAppDispatch } from '../store';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { AccountBox } from '@material-ui/icons';
-import { useSelector } from 'react-redux';
-import { home, login, organization, profile, useNavigate, useQuery } from '../routes';
-import { Token } from '../models';
-import { useLocation } from 'react-router-dom';
 const useStyles = makeStyles(() => ({
   'top-menu': {
     color: 'gray',
@@ -37,36 +32,9 @@ const Header = () => {
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef<HTMLButtonElement>(null);
   const dispatch = useAppDispatch();
-  const userType = useSelector(fromProfile.selectType);
-  const { replaceQuery, navigate, replace } = useNavigate();
-  const  isAuthenticated = useSelector(fromAuth.selectIsAuthenticated);
-  const { get, clear, queryString, queryDictionary } = useQuery();
-  const location = useLocation();
-
-
-  const redirectToProfile = () => {
-    if (userType === 'organization') {
-      navigate(organization.path, replaceQuery({}));
-    } else {
-      navigate(profile.path, replaceQuery({}));
-    }
-  };
-
 
   const handleToggle = () => {
-    (async () => {
-      if (!isAuthenticated) {
-        navigate(
-          login.path,
-          replaceQuery({ redirect: `${location.pathname}?${queryString()}` }),
-        );
-      }
-      const accountType = JSON.parse(localStorage.getItem(fromAuth.TYPE) || 'volunteer');
-      await dispatch(fromAuth.doResume(undefined));
-      await dispatch(fromProfile.doFetchMyProfile({ type: accountType }));
-      setOpen(prevOpen => !prevOpen);
-
-    })();
+    setOpen(prevOpen => !prevOpen);
   };
 
   const handleClose = (event: React.MouseEvent<EventTarget>) => {
@@ -75,7 +43,6 @@ const Header = () => {
 
   const handleLogout = async () => {
     await dispatch(fromAuth.doLogout());
-    navigate(login.path, replaceQuery({}));
   };
 
   function handleListKeyDown(event: React.KeyboardEvent) {
@@ -111,54 +78,50 @@ const Header = () => {
           <Typography style={{ color: '#fa6980', lineHeight: '30px', marginRight: 42 }}>LOGO</Typography>
 
           <Grid item className={classes['top-menu']}>
-            <span
-              onClick={() => {navigate(home.path, replaceQuery({}));}}
-              style={{ color: '#343638' }}>
-              <Typography>Home</Typography>
+            <span style={{ color: '#343638' }}>
+              <Typography>HOME</Typography>
             </span>
             <span>
-              <Typography>Opportunities</Typography>
+              <Typography>OPPORTUNITIES</Typography>
             </span>
             <span>
-              <Typography>About Us</Typography>
+              <Typography>ABOUT US</Typography>
             </span>
           </Grid>
         </Grid>
-
+       
       </Grid>
       <Grid style={{ display: 'flex', alignItems: 'center' }}>
-        <SearchIcon></SearchIcon>
-        <NotificationsIcon style={{ margin:'0 20px' }}></NotificationsIcon>
-        <Avatar onClick={handleToggle} style={{ backgroundColor: 'orange', width: 30, height: 30, marginLeft: 10 }}>
-          <span ref={anchorRef}>N</span>
-        </Avatar>
+          <SearchIcon></SearchIcon>
+          <NotificationsIcon style={{ margin:'0 20px' }}></NotificationsIcon>
+          <Avatar onClick={handleToggle} style={{ backgroundColor: 'orange', width: 30, height: 30, marginLeft: 10 }}>
+            <span ref={anchorRef}>N</span>
+          </Avatar>
 
-        <Popper
-          open={open}
-          anchorEl={anchorRef.current}
-          placement="bottom-end"
-          role={undefined}
-          transition
-          disablePortal
-        >
-          {({ TransitionProps }) => (
-            <Grow {...TransitionProps}>
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList id="menu-list-grow">
-                    <MenuItem onClick={redirectToProfile}>
-                      Profile&nbsp; <AccountBox></AccountBox>
-                    </MenuItem>
-                    <MenuItem onClick={handleLogout}>
-                      Logout&nbsp; <ExitToAppIcon></ExitToAppIcon>
-                    </MenuItem>
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
-      </Grid>
+          <Popper
+            style={{ zIndex:99 }}
+            open={open}
+            anchorEl={anchorRef.current}
+            placement="bottom-end"
+            role={undefined}
+            transition
+            disablePortal
+          >
+            {({ TransitionProps }) => (
+              <Grow {...TransitionProps}>
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList id="menu-list-grow">
+                      <MenuItem onClick={handleLogout}>
+                        Logout&nbsp; <ExitToAppIcon></ExitToAppIcon>
+                      </MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </Grid>
     </div>
   );
 };
